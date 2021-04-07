da33dda602bf46dfc70b10b531cd0ac70456894d
Sergey <sergo205rus@gmail.com>
2021-04-07 19:45:34 +0400

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,74 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+  // код для задачи №1 писать здесь
+  if (a == 0) return false;
+  let x = {};
+  let D = b * b - 4 * a * c;
+  console.log("D = " + D);
+  if (D < 0) {
+    return (x = []);
+  }
+
+  x["discriminant"] = D;
+  if (D == 0) {
+    x["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+  } else if (D > 0) {
+    let tmp = [];
+    tmp.push((-b + Math.sqrt(D)) / (2 * a));
+    tmp.push((-b - Math.sqrt(D)) / (2 * a));
+    x["quadratic roots"] = tmp;
+  }
+  return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  if (marks.length === 0) {
+    return 0;
+  } else if (marks.length > 5) {
+    console.log("Вы ввели более 5 оценок");
+    marks.splice(5);
+  }
+
+  let numberOfRatings = marks.length;
+  let sumRarings = 0;
+
+  for (let i = 0; i < numberOfRatings; i++) {
+    sumRarings += marks[i];
+  }
+
+  let averageMark = sumRarings / numberOfRatings;
+  return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+  let yearOfBirth = dateOfBirthday.getFullYear(), //выводим год из даты рождения
+    monthOfBirth = dateOfBirthday.getMonth(), //выводим месяц
+    dayOfBirth = dateOfBirthday.getDate(); //выводим день
+  let todayDate = new Date();
+
+  let todayYear = todayDate.getFullYear(), //выводим год из текущей даты
+    todayMonth = todayDate.getMonth(),
+    todayDay = todayDate.getDate();
+  let age = 0;
+
+  if (todayMonth > monthOfBirth) {
+    age = todayYear - yearOfBirth;
+  } else if (todayMonth === monthOfBirth) {
+    if (todayDay < dayOfBirth) {
+      age = todayYear - yearOfBirth - 1;
+    } else {
+      age = todayYear - yearOfBirth;
+    }
+  } else {
+    age = todayYear - yearOfBirth - 1;
+  }
+
+  let result = "";
+
+  if (age >= 18) {
+    result = `Не желаете ли олд-фэшн, ${name}?`;
+  } else if (age < 18) {
+    result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+  }
+
+  return result;
 }
