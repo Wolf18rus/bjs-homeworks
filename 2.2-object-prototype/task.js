f92da1dca82a16001c8126e9c5612c4fe546fd40
Sergey <sergo205rus@gmail.com>
2021-04-18 12:52:40 +0400

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,24 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+  let words = this;
+  words = words.toLowerCase().replace(/\s/g, "");
+  return words === words.split("").reverse().join("");
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  // код для задачи №2 писать здесь
+  let average = marks.reduce(function (sum, total) {
+    return sum + total / marks.length;
+  }, 0);
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  // код для задачи №3 писать здесь
+  // let dayBirds = birds.getDay();
+  // monthBirds = birds.getMonth();
+  // yeraBirds = birds.getFullYear();
+  // birthday = dayBirds.getUTCDate();
+  // console.log(birthday);
+  // return verdict;
 }
