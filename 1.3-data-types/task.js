c5d2e6464e2cc9617dca8310e032dd8025a55658
Sergey <sergo205rus@gmail.com>
2021-04-18 12:54:47 +0400

    second commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,6 +2,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   // код для задачи №1 писать здесь
   "use strict";
 
+  if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    contribution === "" ||
+    amount === "" ||
+    date == null ||
+    date == "" ||
+    isNaN(date)
+  ) {
+    return `Пойжалуста заполните все указанные поля ввода `;
+  }
+
   let sum = amount - contribution;
   let procent = percent / 12 / 100;
   let nextYear = date.getFullYear();
@@ -10,23 +23,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     (nextYear - new Date().getFullYear()) * 12 +
     (nextMoth - new Date().getMonth());
   let results;
-  paymentInMonth = sum * (procent + procent / ((1 + procent) ** year - 1));
+  let paymentInMonth = sum * (procent + procent / ((1 + procent) ** year - 1));
   console.log(paymentInMonth);
   results = paymentInMonth * year;
-
-  console.log(Math.ceil(results));
-  let totalAmount = results;
+  let totalAmount = results.toFixed(2);
   return `${totalAmount}`;
 }
 
 function getGreeting(name) {
-  let rep = (this.value = name.replace(/[ ]/));
-
-  if (name === "") {
+  if (name === "" || !isNaN(name) || name === null) {
     let anonim = "Аноним ";
     console.log("В поле только кириллица");
     return ` Привет, мир! Меня зовут${anonim}.`;
-  } else if (name) {
+  } else if (name || name !== "") {
     return ` Привет, мир! Меня зовут-${name}.`;
   }
   //если нужно запретить ввод определёных символов
