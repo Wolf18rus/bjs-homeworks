3ef1e4064bed067365a2da13206aa35dd2ac89a7
Sergey <sergo205rus@gmail.com>
2021-05-22 20:48:50 +0400

    five commit

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,95 +1,92 @@
 // Задание 1
 function getSolutions(a, b, c) {
-  let D = b * b - 4 * a * c;
-  if (D < 0) {
-    return {
-      D: D,
-      roots: [],
-    };
+  let solutions = {
+    // Объявляем массив результатов кв.уравнения
+    D: "",
+    roots: [],
+  };
+
+  if (a === 0) {
+    return a;
   }
 
-  if (D === 0) {
-    let x1 = (-b / 2) * a;
-    return {
-      D: D,
-      roots: [x1],
-    };
-  } else if (D > 0) {
-    let x1 = (-b + Math.sqrt(D)) / (2 * a);
-    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+  solutions.D = Math.pow(b, 2) - 4 * a * c;
 
-    return {
-      D: D,
-      roots: [x1, x2],
-    };
+  if (solutions.D < 0) {
+    return solutions;
+  } else {
+    solutions.roots[0] = (-b + Math.sqrt(solutions.D)) / (2 * a);
+    if (solutions.D > 0) {
+      solutions.roots[1] = (-b - Math.sqrt(solutions.D)) / (2 * a);
+    }
   }
+  return solutions;
 }
 
 function showSolutionsMessage(a, b, c) {
-  let result = getSolutions(1, 5, 4);
-  console.log(
-    `Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}    \n Значение дискриминанта:${result.D}»`
-  );
-  if (result.D < 0) {
-    return ` Уравнение не имеет вещественных корней  `;
-  } else if (result.D === 0) {
-    return ` «Уравнение имеет один корень X₁ = ${result.roots}» `;
-  } else if (result.D > 0) {
-    return ` «Уравнение имеет два  корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}» `;
+  let result = getSolutions(a, b, c); // Получили данные и результаты квадратного уравнения
+  let resultNote = [
+    `Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}\nЗначение дискриминанта: ${result.D}`,
+  ]; // Сформировали текстовой массив для наполнения информацией по решению уравнения
+
+  if (result.roots.length === 0) {
+    resultNote = resultNote.push("Уравнение не имеет вещественных корней");
+  } else if (result.roots.length === 1) {
+    resultNote.push(`\nУравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else {
+    resultNote.push(
+      `\nУравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`
+    );
   }
+
+  return String(resultNote);
 }
-console.log(showSolutionsMessage(1, 5, 4));
 
 // Задание 2
 
-function getAverageMark(...marks) {
-  if (marks.length === 0) {
-    return 0;
-  }
-  let sum = 0;
-  for (let i = 0; i < marks.length; i++) {
-    sum += marks[i];
-  }
-  // let result = marks.reduce((count, calc) => count + calc);
-  return sum / marks.length;
-}
-
-console.log(getAverageMark(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-
 function getAverageScore(data) {
-  let less = {};
-  let valuesData = Object.keys(data);
-  if (data === {}) {
-    less.average = 0;
-  }
+  let averageMarkSubjects = {}; // Создали пустой массив для списка предметов и средних оценок
 
-  if (valuesData.length >= 10) {
-    return `слишком много предметов`;
+  if (Object.keys(data).length === 0) {
+    // Проверка на наличие предметов в массиве
+    averageMarkSubjects.average = 0;
+    return averageMarkSubjects;
   }
 
   for (let key in data) {
     let marks = Object.values(data[key]);
-    less[key] = getAverageMark(...marks);
+    averageMarkSubjects[key] = getAverageMark(marks);
   }
+  let marks = Object.values(averageMarkSubjects);
+  averageMarkSubjects.average = getAverageMark(marks);
 
-  marks = Object.values(less);
-  less.average = getAverageMark(...marks);
-
-  return less;
+  return averageMarkSubjects;
 }
 
-console.log(
-  getAverageScore(
-    (data = {
-      algebra: [4, 5, 5, 4],
-      geometry: [2, 5],
-      russian: [3, 3, 4, 5],
-      physics: [5, 5],
-      music: [2, 2, 5],
-      english: [4, 4, 3, 3],
-      poetry: [5, 3, 4],
-      chemistry: [2],
-      french: [4, 4],
-    })
-  )
-);
+function getAverageMark(marks) {
+  let sumMarks = 0; // Определяем сумму оценок предмета/предметов
+  let averageMark; // Определяем среднюю оценку
+
+  if (marks.length === 0) return 0; // Проверяем массив на наличие оценок
+
+  for (let i = 0; i < marks.length; i++) {
+    sumMarks = marks[i] + sumMarks;
+  }
+  averageMark = sumMarks / marks.length;
+
+  return averageMark;
+}
+
+// getAverageScore(
+//   (data = {
+//     algebra: [4, 5, 5, 4],
+//     geometry: [2, 5],
+//     russian: [3, 3, 4, 5],
+//     physics: [5, 5],
+//     music: [2, 2, 5],
+//     english: [4, 4, 3, 3],
+//     poetry: [5, 3, 4],
+//     chemistry: [2],
+//     french: [4, 4],
+//   })
+// );
